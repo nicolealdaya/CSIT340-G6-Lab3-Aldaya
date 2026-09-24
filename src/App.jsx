1a841028f901817ab60a3da6dac6a5b7401ad9e8
Nicole Sheen Aldaya <sheennicole78@gmail.com>
2026-09-25 03:55:06 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 const Header = (props) => {
  return <h1>{props.course}</h1>
 }
-const App = () => {
- const course = 'CSIT340 - Industry Elective 1'
+const Part = (props) => {
+ return (
+<p>
+     {props.name} {props.exercises} units
+</p>
+ )
+}
+const Content = (props) => {
  return (
 <div>
-<Header course={course} />
+<Part
+       name={props.parts[0].name}
+       exercises={props.parts[0].exercises}
+     />
+<Part
+       name={props.parts[1].name}
+       exercises={props.parts[1].exercises}
+     />
+<Part
+       name={props.parts[2].name}
+       exercises={props.parts[2].exercises}
+     />
+</div>
+ )
+}
+const App = () => {
+ const course = {
+   name: 'CSIT340 - Industry Elective 1',
+   parts: [
+     {
+       name: 'CSIT321 - Applications Development and Emerging Technologies',
+       exercises: 3
+     },
+     {
+       name: 'IT365 - Data Analytics 1',
+       exercises: 3
+     },
+     {
+       name: 'CSIT327 - Information Management 2',
+       exercises: 3
+     }
+   ]
+ }
+ return (
+<div>
+<Header course={course.name} />
+<Content parts={course.parts} />
 </div>
  )
 }
